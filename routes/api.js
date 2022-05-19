0bf30bd2a263c5395f20d52fb369297f1a3f3842
jonahchoi <104597125+jonahchoi@users.noreply.github.com>
2022-05-18 22:38:29 -0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,45 @@
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
+const bodyParser  = require('body-parser');
+
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  //Get request for the form input
+  app.route('/api/convert').get((req, res)=>{
+    let {input} = req.query;
+
+    //Sets variables using convertHandler
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+
+    //Checks if number or unit is invalid
+    if(initNum === 'invalid number' && returnUnit === 'invalid unit'){
+      return res.json('invalid number and unit');
+    }
+    else if(initNum === 'invalid number'){
+      return res.json('invalid number');
+    }
+    else if(returnUnit === 'invalid unit'){
+      return res.json('invalid unit');
+    }
+
+    //Convert the number 
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    
+    //Sets the string using variables
+    let result = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+    
+    return res.json({
+      initNum: initNum,
+      initUnit: initUnit,
+      returnNum: returnNum,
+      returnUnit: returnUnit,
+      string: result
+    })
+  })
+  
 };
